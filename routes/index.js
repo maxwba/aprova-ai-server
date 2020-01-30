fa49e544d156cca6652b7aa82e9e7447575384de
Vini <mvfv@outlook.com>
2020-01-30 11:44:48 -0300

    Primeira mudança

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-/* GET home page test */
+/* GET home page test... */
 router.get('/', (req, res) => {
   res.send('Express is running...');
 });
