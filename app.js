defa80b3c22f20b71d1c90c9b92669f4375930d9
Nykolle Malone <nykolle.malone@hotmail.com>
2020-02-05 18:09:25 -0300

    initial commit to heroku

--- app.js
+++ app.js
@@ -84,4 +84,9 @@ app.listen(process.env.PORT, () =>
   console.log(`Listening on Port: ${process.env.PORT}`)
 );
 
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 module.exports = app;
