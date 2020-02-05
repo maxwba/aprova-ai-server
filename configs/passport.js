af6ca8bf4cdc9dddd10e18f6c79473feedca42c4
Nykolle Malone <nykolle.malone@hotmail.com>
2020-02-05 10:53:04 -0300

    passport authentication with google complete

--- configs/passport.js
+++ configs/passport.js
@@ -1,9 +1,12 @@
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const Company = require('../models/company');
 
+
 passport.serializeUser((loggedInCompany, cb) => {
+  // eslint-disable-next-line no-underscore-dangle
   cb(null, loggedInCompany._id);
 });
 
@@ -42,3 +45,31 @@ passport.use(
     },
   ),
 );
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: '/api/auth/google/callback',
+},
+(accessToken, refreshToken, profile, done) => {
+  Company.findOne({
+    googleID: profile.id
+  })
+    .then((company) => {
+      if (company) {
+        done(null, company);
+        return;
+      }
+
+      Company.create({
+        googleID: profile.id,
+        name: profile.displayName,
+        email: profile.emails[0].value,
+      })
+        .then((newUser) => {
+          done(null, newUser);
+        })
+        .catch((err) => done(err)); // closes User.create()
+    })
+    .catch((err) => done(err)); // closes User.findOne()
+}));
