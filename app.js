4e9036fc0e88ab0974f4dafbc53f403cd06aaedc
Nykolle Malone <nykolle.malone@hotmail.com>
2020-02-05 18:21:43 -0300

    chenged the link to server

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use(passport.session());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000"] // <== this will be the URL of our React app (it will be running on port 3000)
+    origin: [process.env.SHARE] // <== this will be the URL of our React app (it will be running on port 3000)
   })
 );
 
