854c77357117377b8371b7f3e5747be5276c3361
Nykolle Malone <nykolle.malone@hotmail.com>
2020-02-05 18:42:01 -0300

    Build in the house

---
+++ public/precache-manifest.d739a11fe40ebffea0a0aa37df589ca8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ae90cc6bcf381a115817b5b24603ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0210909556d78f2b8bd",
+    "url": "/static/css/2.f03e2dd1.chunk.css"
+  },
+  {
+    "revision": "30fa33de2f4d4957d2d2",
+    "url": "/static/css/main.d8e32b52.chunk.css"
+  },
+  {
+    "revision": "e0210909556d78f2b8bd",
+    "url": "/static/js/2.589eb199.chunk.js"
+  },
+  {
+    "revision": "ecf1debcab34f9e47a37376b28c4c326",
+    "url": "/static/js/2.589eb199.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30fa33de2f4d4957d2d2",
+    "url": "/static/js/main.0d0d0766.chunk.js"
+  },
+  {
+    "revision": "8ba56f3c99e266ff4b62",
+    "url": "/static/js/runtime-main.ba8f64b0.js"
+  }
+]);
