cad0123e1bb5326579fe3f42596809a26928bb51
Nykolle Malone <nykolle.malone@hotmail.com>
2020-02-06 09:37:40 -0300

    fixing bugs

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d739a11fe40ebffea0a0aa37df589ca8.js"
+  "/precache-manifest.9dc42c812fa2887b782dbe25cc7960f6.js"
 );
 
 self.addEventListener('message', (event) => {
